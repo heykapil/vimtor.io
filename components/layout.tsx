7c5b2fbcfeabf756798fcd53b73f90f87caf4470
Victor Navarro <victor@vimtor.io>
2021-07-28 00:23:33 +0200

    feat: add common tailwind global utilities

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,14 +10,14 @@ interface LayoutProps {
 
 const Layout = ({ children, title, description }: LayoutProps) => {
   return (
-    <div>
+    <div className="subpixel-antialiased text-gray-900">
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800" />
       </Head>
-      <main>{children}</main>
+      <main className="bg-white">{children}</main>
       <Footer />
     </div>
   );
