3d665f3d3fada2c612a4d38566931de9efd10da2
Victor Navarro <victor@vimtor.io>
2021-08-17 18:11:40 +0200

    fix: smooth and proper #contact navigation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="en">
+            <Html lang="en" className="scroll-behavior-smooth scroll-padding-top-24">
                 <Head>
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
