3e96636cb55930dfe63026b70aed602be72c4d64
vimtor <victor@vimtor.io>
2021-12-11 16:23:06 +0100

    Improve project section styles

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -19,7 +19,7 @@ interface EmptyMessageProps {
 const EmptyMessage = ({ shownCount }: EmptyMessageProps) => {
     if (shownCount === 0) {
         return (
-            <p>
+            <p className="text-xl py-8">
                 Oops, seems like you want me to work a bit too much <Emoji label="flushed face" icon="😳" />
             </p>
         );
@@ -27,7 +27,7 @@ const EmptyMessage = ({ shownCount }: EmptyMessageProps) => {
 
     if (shownCount === 1) {
         return (
-            <p>
+            <p className="text-xl py-8">
                 I haven&apos;t build that yet <Emoji label="smiling face with sunglasses" icon="😎" />
             </p>
         );
@@ -35,7 +35,7 @@ const EmptyMessage = ({ shownCount }: EmptyMessageProps) => {
 
     if (shownCount === 2) {
         return (
-            <p>
+            <p className="text-xl py-8">
                 Are you that interested?
                 <br />
                 We can <Link href="/#contact">build that together</Link>
@@ -45,19 +45,19 @@ const EmptyMessage = ({ shownCount }: EmptyMessageProps) => {
 
     if (shownCount === 3) {
         return (
-            <p>
+            <p className="text-xl py-8">
                 We can get married if you insist <Emoji label="wedding ring" icon="💍" />
             </p>
         );
     }
 
     if (shownCount === 4) {
-        return <p>I&apos;m sure you have better things to do...</p>;
+        return <p className="text-xl py-8">I&apos;m sure you have better things to do...</p>;
     }
 
     return (
-        <p>
-            I leave you with <a href="https://www.youtube.com/watch?v=4dC_nRYIDZU">something interesting</a> to watch.
+        <p className="text-xl py-8">
+            I leave you with <Link href="https://www.youtube.com/watch?v=4dC_nRYIDZU">something interesting</Link> to watch.
             <br />
             Sayonara baby!
         </p>
