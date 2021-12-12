15ea5819b647e22f0d53e171f438fcd64dbf45d8
vimtor <victor@vimtor.io>
2021-12-12 19:53:57 +0100

    Fix horizontal overflow on rocket animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Footer from "../components/footer";
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <div className="subpixel-antialiased text-gray-900">
-            <div className="lg:grid lg:grid-rows-layout min-h-screen">
+            <div className="lg:grid lg:grid-rows-layout min-h-screen overflow-x-hidden">
                 <Navbar />
                 <main className="bg-white">
                     <Component {...pageProps} />
