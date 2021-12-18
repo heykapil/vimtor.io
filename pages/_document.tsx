2aa0650560c582f15b121798ad2550bfbd719ce5
vimtor <victor@vimtor.io>
2021-12-18 13:01:02 +0100

    Add new favicon and clean images

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render() {
         return (
             <Html lang="en" className="scroll-smooth scroll-pt-24">
                 <Head>
-                    <link rel="icon" href="/favicon.ico" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <link rel="manifest" href="/site.webmanifest" />
+                    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cd9147" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta name="theme-color" content="#ffffff" />
                     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
