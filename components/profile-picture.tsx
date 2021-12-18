dfecf76e0843fe3b1737bbd7e6171f22b9c669aa
vimtor <victor@vimtor.io>
2021-12-18 12:19:26 +0100

    Profile picture bubble delay

--- components/profile-picture.tsx
+++ components/profile-picture.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import MessageBubble from "./message-bubble";
 import ImageShadow from "./image-shadow";
@@ -6,8 +5,9 @@ import profile1 from "../public/images/profile.jpg";
 import profile2 from "../public/images/profile-2.png";
 import profile3 from "../public/images/profile-3.png";
 import { Transition } from "@headlessui/react";
+import { useCounter } from "react-use";
 
-const PROFILE_IMAGES = [profile1, profile2, profile3];
+const profileImages = [profile1, profile2, profile3];
 
 interface ProfileImageProps {
     src: StaticImageData;
@@ -24,7 +24,7 @@ const ImageSlide = ({ src, visible }: ProfileImageProps) => {
 };
 
 const ProfilePicture = () => {
-    const [currentProfileIndex, setCurrentProfileIndex] = useState(0);
+    const [currentProfileIndex, { inc: incrementProfileIndex }] = useCounter(0);
     const profileHasChanged = currentProfileIndex > 0;
 
     return (
@@ -33,13 +33,10 @@ const ProfilePicture = () => {
             <button
                 aria-describedby="profile-bubble"
                 className="transition-all rounded-full opacity-0 motion-safe:animate-bounce-in overflow-hidden w-[200px] h-[200px] outline-none ring-gray-900 focus:ring-4 hover:ring-4 ring-opacity-80"
-                onClick={() => {
-                    const newIndex = (currentProfileIndex + 1) % PROFILE_IMAGES.length;
-                    setCurrentProfileIndex(newIndex);
-                }}
+                onClick={() => incrementProfileIndex()}
             >
-                {PROFILE_IMAGES.map((src, index) => (
-                    <ImageSlide key={index} src={src} visible={currentProfileIndex === index} />
+                {profileImages.map((src, index) => (
+                    <ImageSlide key={index} src={src} visible={currentProfileIndex % profileImages.length === index} />
                 ))}
             </button>
         </div>
