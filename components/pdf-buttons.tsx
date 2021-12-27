d07e734ab3a4125654ace9bd877b801224d9f407
vimtor <victor@vimtor.io>
2021-12-27 11:13:25 +0100

    Resume section improvements

--- components/pdf-buttons.tsx
+++ components/pdf-buttons.tsx
@@ -6,29 +6,25 @@ import { classNames } from "../utils/style";
 interface PdfButtonsProps extends Omit<ComponentProps<typeof PDFDownloadLink>, "children" | "style"> {}
 
 function PdfButtons({ document, fileName, className }: PdfButtonsProps) {
-    const [{ url, loading }] = usePDF({ document });
-
-    if (loading || !url) {
-        return null;
-    }
+    const [{ url }] = usePDF({ document });
 
     return (
         <div className={classNames("sm:flex max-w-[50vh] px-4 sm:max-w-none sm:gap-3 mx-auto space-y-3 sm:space-y-0 justify-center items-center", className)}>
             <a
                 download={fileName}
-                href={url}
-                className="w-full sm:w-auto inline-flex justify-center items-center px-6 py-3 border border-transparent shadow-sm text-lg font-medium rounded-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+                href={url || ""}
+                className="w-full sm:w-auto inline-flex justify-center items-center px-6 py-3 border border-2 border-transparent shadow-sm text-lg font-medium rounded-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
             >
                 <DownloadIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
-                Download File
+                Download
             </a>
             <a
-                href={url}
+                href={url || ""}
                 target="_blank"
                 rel="noreferrer"
-                className="w-full sm:w-auto inline-flex justify-center items-center px-6 py-3 border border-transparent shadow-sm text-lg font-medium rounded-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
+                className="w-full sm:w-auto inline-flex justify-center items-center px-6 py-3 border border-2 border-gray-800 shadow-sm text-lg font-medium rounded-full text-gray-800 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600"
             >
-                View
+                View PDF
             </a>
         </div>
     );
