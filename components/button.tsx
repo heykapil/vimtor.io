bcba5d4c9099c45c80e1f7433bb91a25d26245da
vimtor <victor@vimtor.io>
2022-02-14 23:06:09 +0100

    Fix console errors

--- components/button.tsx
+++ components/button.tsx
@@ -1,25 +1,24 @@
 import { classNames } from "../lib/style";
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 
 interface ButtonProps extends ComponentProps<"a"> {
     variant: "primary" | "secondary";
     size: "small" | "medium";
 }
 
-function Button({ variant, size, className, ...props }: ButtonProps) {
-    return (
-        <a
-            {...props}
-            className={classNames(
-                "inline-flex justify-center items-center border border-2 border-transparent shadow-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2",
-                variant === "primary"
-                    ? "text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-600"
-                    : "text-gray-800 bg-white border-gray-800 hover:bg-gray-100 focus:ring-gray-600",
-                size === "medium" ? "px-6 py-3 text-lg" : "text-lg px-6 py-2",
-                className
-            )}
-        />
-    );
-}
+const Button = forwardRef<HTMLAnchorElement, ButtonProps>(({ variant, size, className, ...props }, ref) => (
+    <a
+        {...props}
+        ref={ref}
+        className={classNames(
+            "inline-flex justify-center items-center border border-2 border-transparent shadow-sm font-medium rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2",
+            variant === "primary"
+                ? "text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-600"
+                : "text-gray-800 bg-white border-gray-800 hover:bg-gray-100 focus:ring-gray-600",
+            size === "medium" ? "px-6 py-3 text-lg" : "text-lg px-6 py-2",
+            className
+        )}
+    />
+));
 
 export default Button;
