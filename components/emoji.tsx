5b1e7645093efb11508cd1e9c05b2bb840147444
vimtor <victor@vimtor.io>
2022-07-02 12:04:06 +0200

    Make emoji reappear after being off the screen

--- components/emoji.tsx
+++ components/emoji.tsx
@@ -1,15 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import { classNames } from "../lib/style";
+import useElementVisible from "../hooks/use-element-visible";
 
 const animations = {
     wiggle: "animate-wiggle",
     rocket: "animate-rocket",
 };
 
+type AnimationName = keyof typeof animations;
+
 interface EmojiProps {
     label: string;
     icon: string;
-    animation?: keyof typeof animations;
+    animation?: AnimationName;
     reset?: boolean;
     appear?: boolean;
     delay?: number;
@@ -17,6 +20,7 @@ interface EmojiProps {
 
 function Emoji({ label, icon, delay, animation = "wiggle", reset = true, appear = false }: EmojiProps) {
     const [hovered, setHovered] = useState(false);
+    const [ref, visible] = useElementVisible()
 
     const playAnimation = () => {
         if (!hovered) {
@@ -31,12 +35,19 @@ function Emoji({ label, icon, delay, animation = "wiggle", reset = true, appear 
 
     useEffect(() => {
         if (appear) {
-            setTimeout(playAnimation, delay || 0);
+            setTimeout(playAnimation, delay ?? 0);
         }
     }, [appear, delay]);
 
+    useEffect(() => {
+        if (!reset && !visible) {
+            setHovered(false);
+        }
+    }, [reset, visible])
+
     return (
         <span
+            ref={ref}
             className={classNames("inline-block font-emoji", hovered ? animations[animation] : "")}
             role="img"
             aria-label={label}
