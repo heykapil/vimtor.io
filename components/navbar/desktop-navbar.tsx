1cbb84c5608ecf4c45a49949ccfa30c7f598bb98
vimtor <victor@vimtor.io>
2022-08-20 21:24:20 +0200

    Add link to blog in the navbar

--- components/navbar/desktop-navbar.tsx
+++ components/navbar/desktop-navbar.tsx
@@ -5,87 +5,83 @@ import { useRouter } from "next/router";
 import { classNames } from "../../lib/style";
 
 interface NavbarItemProps {
-    href: string;
-    children: string;
+  href: string;
+  children: string;
 }
 
 const container = {
-    hidden: { opacity: 0 },
-    show: {
-        opacity: 1,
-        transition: {
-            delayChildren: 0.5,
-            staggerChildren: 0.1,
-        },
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
     },
+  },
 };
 
 const item = {
-    hidden: { opacity: 0, y: 8 },
-    show: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.5,
-        },
+  hidden: { opacity: 0, y: 8 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
     },
+  },
 };
 
 function NavbarItem({ href, children }: NavbarItemProps) {
-    const { pathname } = useRouter();
+  const { pathname } = useRouter();
 
-    const active = pathname === href;
+  const active = pathname === href;
 
-    return (
-        <motion.li variants={item}>
-            <Link href={href} passHref>
-                <a
-                    className={classNames(
-                        "transition-color text-lg hover:text-gray-900 outline-gray-800 relative group",
-                        active ? "text-gray-900" : "text-gray-400"
-                    )}
-                >
-                    {children}
-                    <span
-                        className={classNames(
-                            "absolute left-0 -bottom-2 transition-all group-hover:w-full group-hover:opacity-100 bg-gray-600 h-0.5 duration-300",
-                            active ? "w-full opacity-100" : "w-0 opacity-0"
-                        )}
-                    />
-                </a>
-            </Link>
-        </motion.li>
-    );
+  return (
+    <motion.li variants={item}>
+      <Link href={href} passHref>
+        <a
+          className={classNames(
+            "transition-color text-lg hover:text-gray-900 outline-gray-800 relative group",
+            active ? "text-gray-900" : "text-gray-400"
+          )}
+        >
+          {children}
+          <span
+            className={classNames(
+              "absolute left-0 -bottom-2 transition-all group-hover:w-full group-hover:opacity-100 bg-gray-600 h-0.5 duration-300",
+              active ? "w-full opacity-100" : "w-0 opacity-0"
+            )}
+          />
+        </a>
+      </Link>
+    </motion.li>
+  );
 }
 
 function DesktopNavbar() {
-    return (
-        <div className="flex items-center">
-            <div className="hidden sm:block">
-                <motion.ul
-                    variants={container}
-                    initial="hidden"
-                    animate="show"
-                    className="flex items-baseline space-x-8"
-                >
-                    <NavbarItem href="/projects">Projects</NavbarItem>
-                    <NavbarItem href="/technologies">Technologies</NavbarItem>
-                    <NavbarItem href="/resume">Resume</NavbarItem>
-                    <motion.li
-                        initial={{ scale: 0.5, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{ duration: 0.7, delay: 1, ease: [0.15, 1.5, 0.5, 1] }}
-                    >
-                        <Link href="/contact" passHref>
-                            <Button variant="primary" size="small">
-                                Contact
-                            </Button>
-                        </Link>
-                    </motion.li>
-                </motion.ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center">
+      <div className="hidden sm:block">
+        <motion.ul variants={container} initial="hidden" animate="show" className="flex items-baseline space-x-8">
+          <NavbarItem href="/projects">Projects</NavbarItem>
+          <NavbarItem href="/blog">Articles</NavbarItem>
+          <NavbarItem href="/technologies">Technologies</NavbarItem>
+          <NavbarItem href="/resume">Resume</NavbarItem>
+          <motion.li
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.7, delay: 1, ease: [0.15, 1.5, 0.5, 1] }}
+          >
+            <Link href="/contact" passHref>
+              <Button variant="primary" size="small">
+                Contact
+              </Button>
+            </Link>
+          </motion.li>
+        </motion.ul>
+      </div>
+    </div>
+  );
 }
 
 export default DesktopNavbar;
--- components/navbar/mobile-navbar.tsx
+++ components/navbar/mobile-navbar.tsx
@@ -4,43 +4,44 @@ import { useRouter } from "next/router";
 import { classNames } from "../../lib/style";
 
 interface NavbarItemProps {
-    children: string;
-    href: string;
+  children: string;
+  href: string;
 }
 
 function NavbarItem({ children, href }: NavbarItemProps) {
-    const { pathname } = useRouter();
+  const { pathname } = useRouter();
 
-    const active = pathname === href;
+  const active = pathname === href;
 
-    return (
-        <Link href={href} passHref>
-            <a className="block outline-none ring-gray-800 rounded-md ring-offset-2 focus:ring-2">
-                <Disclosure.Button
-                    as="span"
-                    className={classNames(
-                        "text-center text-lg border block p-3 rounded-md text-base font-medium",
-                        active ? "border-gray-800 bg-gray-800 text-white" : "border-gray-300 shadow-inner text-gray-400"
-                    )}
-                >
-                    {children}
-                </Disclosure.Button>
-            </a>
-        </Link>
-    );
+  return (
+    <Link href={href} passHref>
+      <a className="block outline-none ring-gray-800 rounded-md ring-offset-2 focus:ring-2">
+        <Disclosure.Button
+          as="span"
+          className={classNames(
+            "text-center text-lg border block p-3 rounded-md text-base font-medium",
+            active ? "border-gray-800 bg-gray-800 text-white" : "border-gray-300 shadow-inner text-gray-400"
+          )}
+        >
+          {children}
+        </Disclosure.Button>
+      </a>
+    </Link>
+  );
 }
 
 function MobileNavbar() {
-    return (
-        <Disclosure.Panel className="sm:hidden absolute bg-white shadow-md w-full">
-            <div className="px-3 pt-2 pb-5 space-y-3">
-                <NavbarItem href="/projects">Projects</NavbarItem>
-                <NavbarItem href="/technologies">Technologies</NavbarItem>
-                <NavbarItem href="/resume">Resume</NavbarItem>
-                <NavbarItem href="/contact">Contact</NavbarItem>
-            </div>
-        </Disclosure.Panel>
-    );
+  return (
+    <Disclosure.Panel className="sm:hidden absolute bg-white shadow-md w-full">
+      <div className="px-3 pt-2 pb-5 space-y-3">
+        <NavbarItem href="/projects">Projects</NavbarItem>
+        <NavbarItem href="/blog">Articles</NavbarItem>
+        <NavbarItem href="/technologies">Technologies</NavbarItem>
+        <NavbarItem href="/resume">Resume</NavbarItem>
+        <NavbarItem href="/contact">Contact</NavbarItem>
+      </div>
+    </Disclosure.Panel>
+  );
 }
 
 export default MobileNavbar;
